68c2467c22d3f52f503e5da7cdfd96bdf14ae19b
denniscava <denniscava7@gmail.com>
2017-06-11 17:05:20 -0500

    se creo el metodo index en el controlador y se esta haciendo la vista

--- app/Http/Controllers/BudgetController.php
+++ app/Http/Controllers/BudgetController.php
@@ -3,15 +3,21 @@
 namespace Budgets\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Budgets\Http\Budget;
+use Budgets\Budget;
 
 class BudgetController extends Controller
 {
-    //
+    public function create()
+	{
+		$budget = new Budget;
+		return view('budgets.create')->with(['budget' => $budget]);
+	}
+
+	public function index()
+    {
+    	$budget = Budget::orderBy('id', 'DESC')->paginate();
+    	return view('budgets.index', compact('budget'));
+    }
+
 }
 
-public function create()
-{
-	$budget = new Budget;
-	return view('budgets.create')->with(['budget' => $budget]);
-}
---
+++ resources/views/budgets/index.blade.php
@@ -0,0 +1,17 @@
+@extends ('layouts.app')
+
+@section('content')
+	
+		
+		<div class="row">
+		  <div class="col-md-12">
+		    <h2>Presupuesto 1
+		      
+		    </h2>
+		    
+		  </div>
+		</div>
+		
+	
+	
+@endsection
