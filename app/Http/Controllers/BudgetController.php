7baef797539239ca81121d303b6ab3a8ae8469ba
tibecvp <tibecvp@gmail.com>
2017-06-11 15:48:24 -0500

    Se agrega método create en BudgetController.php y la vista para crear budgets

--- app/Http/Controllers/BudgetController.php
+++ app/Http/Controllers/BudgetController.php
@@ -3,8 +3,15 @@
 namespace Budgets\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Budgets\Http\Budget;
 
 class BudgetController extends Controller
 {
     //
+}
+
+public function create()
+{
+	$budget = new Budget;
+	return view('budgets.create')->with(['budget' => $budget]);
 }
