ff28841d7ec65bd8a0a0485a34a7422396885e1b
tibecvp <tibecvp@gmail.com>
2017-06-13 15:39:16 -0500

    Se implementa la tabla en la vista show de cada presupuesto

--- resources/views/budgets/show.blade.php
+++ resources/views/budgets/show.blade.php
@@ -10,13 +10,64 @@
       	</small>
 	</h1>
 	<p>{{$budget->description}}</p>
-	<div>
-	<ul>
-		@foreach($categories as $category)
-			<li>
-				<a href="{{route('categories.edit', ['category' => $category->id])}}">{{$category->name}}</a>
-			</li>
-		@endforeach
-	</ul>
+	<div class="login-box-body">
+		<table class="table">
+			<thead>
+				<th>
+
+				</th>
+					<th>
+						Ene
+					</th>
+					<th>
+						Feb
+					</th>
+					<th>
+						Mar
+					</th>
+					<th>
+						Abr
+					</th>
+					<th>
+						May
+					</th>
+					<th>
+						Jun
+					</th>
+					<th>
+						Jul
+					</th>
+					<th>
+						Ago
+					</th>
+					<th>
+						Sep
+					</th>
+					<th>
+						Oct
+					</th>
+					<th>
+						Nov
+					</th>
+					<th>
+						Dic
+					</th>
+					<th>
+						<b>TOTAL</b>
+					</th>
+			</thead>
+			<tbody>
+				@foreach($categories as $category)
+						<tr>
+							<td width="20%">
+								<b><a href="{{route('categories.edit', ['category' => $category->id])}}">{{$category->name}}</a></b>
+							</td>
+							<td colspan="13">
+
+							</td>
+						</tr>
+				@endforeach
+			</tbody>
+		</table>
 	</div>
 @endsection
